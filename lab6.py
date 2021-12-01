2c4fe775167afe35d65f50c723455af2e21a6e2a
abishek07bimali <abi0shekbimali@gmail.com>
2021-12-01 13:31:52 +0545

    lab2

---
+++ lab6.py
@@ -0,0 +1,12 @@
+# add even number of first 10 oisitive integer
+# add odd number of first 10 oisitive integer
+
+#even number
+listnum=[2,4,6,8,10]
+sumB=sum(listnum)
+print(sumB)
+
+#odd number
+listnum=[1,3,5,7,9]
+sumA=sum(listnum)
+print(sumA)

1318486004cf6f18c6614b66577914e988850293
abishek07bimali <abi0shekbimali@gmail.com>
2021-12-03 12:26:29 +0545

    labwork

--- lab2.py
+++
@@ -1,5 +0,0 @@
-# area of triangle
-Height=int(input("Enter the length in cm :"))
-Base=int(input("Enter the breadth in cm :"))
-Area=(Height*Base)//2
-print("The area of triangle is {} cm\u00b2: ".format(Area))
--- lab5.py
+++
@@ -1,5 +0,0 @@
-# area of circle
-A=22/7
-radius=int(input("Enter the radius in cm :"))
-Area=A*(radius**2)
-print("The area of circle is {} cm\u00b2: ".format(Area))

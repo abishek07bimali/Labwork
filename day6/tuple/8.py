135f43e1694d3a2000d7dbfdc04a59a34ac5a630
abishek07bimali <abi0shekbimali@gmail.com>
2021-12-25 13:11:28 +0545

    update

--- day6/tuple/8.py
+++ day6/tuple/8.py
@@ -1,1 +1,9 @@
-#Write a Python program to create the colon of a tuple. 
+#8. Write a Python program to convert a tuple to a string. 
+ 
+def convert(tup):
+    str = ''.join(tup)
+    return str
+
+
+A=(1,2,3,4,5,)
+print(convert(A))
